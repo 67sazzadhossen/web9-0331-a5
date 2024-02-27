13b0fad4124d1c2b822ef5cb100f9c94c7a99835
67sazzadhossen <134128807+67sazzadhossen@users.noreply.github.com>
2024-02-27 09:45:41 +0600

    adding js

---
+++ functionality.js
@@ -0,0 +1,13 @@
+const perSeatPrice = 550; // Set the per-seat price
+const maxSeatsAllowed = 4; // Set the maximum number of seats allowed
+
+function seatSelection(seatId) {
+    const selectedSeat = document.getElementById(seatId);
+    // selectedSeat.classList.add('bg-[#1DD100]');
+    
+    selectedSeat.classList.add('bg-[#1DD100]');
+    selectedSeat.classList.remove('bg-[#F7F8F8]');
+   
+    
+   
+}
